156e908df5a7b0eb581c7b751494e4b1cad0a343
Emma-Key <emma.key3@nhs.net>
2026-06-18 11:23:55 +0000

    end of mornign course

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,21 @@
 /*
-Example lesson stub
+My first SQL lesson
+test text
+test
 */
+-- This is my select statment
 
 SELECT
-	*
+	ps.PatientId
+	,ps.Hospital
+	,DATEADD(WEEK, -2, PS.AdmittedDate) AS ReminderDate
+	,ps.Ward
+	,ps.AdmittedDate
+	,ps.DischargeDate
+	,ps.Tariff
 FROM
-	PatientStay ps ;
+	PatientStay as ps
+	WHERE ps.Hospital IN ('Kingston','PRUH')
+	AND ps.Ward LIKE '%surgery'
+	AND ps.AdmittedDate BETWEEN '2024-02-27' AND '2024-02-29'
+
